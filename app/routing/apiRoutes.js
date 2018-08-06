ea31d90ab5bf6a14791cb7153b97755f1e7a0af4
Noah <noah@gangplankhq.com>
2018-08-06 14:22:36 -0700

    finished, except modals

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,25 +1,15 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var path = require('path');
+module.exports = function(app){
 
-var peopleData = require('../data/friends')
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.get("/api/people", function(req, res) {
-    res.json(peopleData);
-});
-
-app.get("/survey", function(req, res){
-    return res.json(peopleData)
- });
+    var peopleData = require('../data/friends')
+    
+    app.get("/api/people", function(req, res) {
+        res.json(peopleData);
+    });
+    
+    app.post("/api/people", function(req, res) {
  
+       peopleData.push(req.body)
+    });
+}
 
-app.post("/api/people", function(req, res) {
-   peopleData.push(req.body)
-   console.log(people)
-});
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,26 +1,17 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var path = require('path');
+var path = require("path");
 
-var PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
+module.exports = function(app){
 app.get("/", function(req, res) {
-    res.sendFile(path.resolve("../public/home.html"));
+    res.sendFile(path.resolve("./public/home.html"));
 });
 
 app.get("/survey", function(req, res){
-    res.sendFile(path.resolve("../public/survey.html"));
+    res.sendFile(path.resolve("./public/survey.html"));
 });
 
 app.get("/matches", function(req, res){
-    res.sendFile(path.resolve("../public/matches.html"));
+    res.sendFile(path.resolve("./public/matches.html"));
 });
+}
 
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
